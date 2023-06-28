44e62445a9f530c30a277f006a58ecaf1feff5ae
annabenari <annabenari@me.com>
2023-06-28 13:59:45 +0100

    second commit of 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -72,9 +72,13 @@ describe("GET /api/articles/:article_id", () => {
       .get(`/api/articles/${existingArticleId}`)
       .expect(200)
       .then((response) => {
+        expect(Array.isArray(response.articles)).toBe(false);
         expect(response.body.articles[0]).toHaveProperty("author");
         expect(response.body.articles[0]).toHaveProperty("title");
-        expect(response.body.articles[0]).toHaveProperty("article_id");
+        expect(response.body.articles[0]).toHaveProperty(
+          "article_id",
+          existingArticleId
+        );
         expect(response.body.articles[0]).toHaveProperty("body");
         expect(response.body.articles[0]).toHaveProperty("topic");
         expect(response.body.articles[0]).toHaveProperty("created_at");
@@ -84,8 +88,32 @@ describe("GET /api/articles/:article_id", () => {
   });
 });
 
+test("should return the article details for an existing article", () => {
+  const anotherExistingArticleId = 1;
+
+  return request(app)
+    .get(`/api/articles/${anotherExistingArticleId}`)
+    .expect(200)
+    .then((response) => {
+      expect(Array.isArray(response.articles)).toBe(false);
+    });
+});
+
 describe("GET /api/articles/", () => {
   test("should return a 200 status code", () => {
     return request(app).get("/api/articles/").expect(200);
+  });
+
+  test("should return an array", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .expect((response) => {
+        expect(Array.isArray(response.body.articles)).toBe(true);
+        expect(response.body.articles.length).toBeGreaterThan(0);
+        response.body.articles.forEach((article) => {
+          expect(typeof article).toBe("object");
+        });
+      });
   });
 });
